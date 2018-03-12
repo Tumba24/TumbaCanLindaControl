16a4ba3a592a697b55c21458c369f7ebc3b9d68a
tumba <tumbanotfound@gmail.com>
2018-03-12 14:34:35 -0600

    Proj created

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TumbaCanLindaControl
+{
+    public class Program
+    {
+        public const string USAGE = @"Usage:
+coincontrol {address} {privateKey} {frequencyInMilliseconds}";
+
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1 || 
+                args[0].Equals("h", StringComparison.InvariantCultureIgnoreCase) ||
+                args[0].Equals("help", StringComparison.InvariantCultureIgnoreCase) ||
+                args[0].Equals("?"))
+            {
+                Console.WriteLine(USAGE);
+                return;
+            }
+        }
+    }
+}
