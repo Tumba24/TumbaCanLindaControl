85eb03a57c699f8f87939838f3220bce836aac2d
tumba <tumbanotfound@gmail.com>
2018-03-16 17:07:20 -0600

    Added scripts to run Win10 coin control.

---
+++ Scripts/Win10/StartCoinControl_build.bat
@@ -0,0 +1,3 @@
+@echo off
+..\..\TumbaCanLindaControl.exe coincontrol user password addressLabel 60000
+pause
---
+++ Scripts/Win10/StartLinda_build.bat
@@ -0,0 +1,2 @@
+@echo off
+start ..\..\..\Linda-qt.exe -server=1 -rpcuser=user -rpcpassword=password -rpcallowip=127.0.0.1 -rpcport=33821
